9f0061340b66bc35aee385ee4833f93a753cebad
Karll <hankikab@gmail.com>
2021-05-03 21:09:15 +0900

    - Car 클래스 추가

---
+++ src/main/java/Car.java
@@ -0,0 +1,21 @@
+public class Car {
+    private String name;
+    private int step;
+
+    public Car(String name) {
+        this.name = name;
+        this.step = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void increaseStep() {
+        this.step++;
+    }
+}
