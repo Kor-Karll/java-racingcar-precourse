1aa9cee47247b439e9534ffc4766e050b45a4e6e
Karll <hankikab@gmail.com>
2021-05-03 22:11:03 +0900

    - CarRacing 클래스 추가

---
+++ src/main/java/CarRacing.java
@@ -0,0 +1,13 @@
+public class CarRacing {
+    Message message;
+    int tryNumber;
+
+    public CarRacing(Message message) {
+        this.message = message;
+        this.tryNumber = 0;
+    }
+
+    public boolean isRunning() {
+        return this.tryNumber == 0;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Message message = new Message();
+        CarRacing carRacing = new CarRacing(message);
     }
 }
