56afab4106bd88cb29d54f69cd82eabc6933dabf
Karll <hankikab@gmail.com>
2021-10-12 09:27:02 +0900

    - CarRacing 클래스 추가

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,10 @@
 package racinggame;
 
+import nextstep.utils.Message;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 자동차 경주 게임 구현
+        Message message = new Message();
+        CarRacing carRacing = new CarRacing(message);
     }
 }
---
+++ src/main/java/racinggame/CarRacing.java
@@ -0,0 +1,17 @@
+package racinggame;
+
+import nextstep.utils.Message;
+
+public class CarRacing {
+    Message message;
+    int tryNumber;
+
+    public CarRacing(Message message) {
+        this.message = message;
+        this.tryNumber = 0;
+    }
+
+    public boolean isRunning() {
+        return this.tryNumber == 0;
+    }
+}
